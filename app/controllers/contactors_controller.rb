969364e7de18c482e4bff36c43e357601ad06cbd
akimono <frank.cioppettini@gmail.com>
2013-06-16 02:15:15 -0400

    adflalf

--- app/controllers/contactors_controller.rb
+++ app/controllers/contactors_controller.rb
@@ -5,7 +5,7 @@ class ContactorsController < ApplicationController
 	end
 	def create
 		@cart = current_cart
-		@contactor = params[contactoremail]
+		@contactor = params[:contactoremail]
 			ContactMailer.contact_email(@contactor).deliver
 			flash[:notice] = "Message has been sent"
 			redirect_to root_path
