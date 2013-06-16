7c043bdd374f2f5bfcdf39aa38ec46e94655cace
akimono <frank.cioppettini@gmail.com>
2013-06-16 01:35:57 -0400

    adfm

--- app/mailers/contact_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ContactMailer < ActionMailer::Base
-	default :from => "contact@akimono.com"
-def contact_email(contactor)
-	@contactor = contactor
-    mail(:to => "contact@akimono.com", :subject => contactor.contactorsubject)
-end
-end
