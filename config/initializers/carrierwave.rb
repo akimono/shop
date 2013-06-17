14a2cf9ab2596cf8ae0b4e7cb6631d4b345ea88f
akimono <frank.cioppettini@gmail.com>
2013-06-17 17:51:07 -0400

    ninth

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -26,7 +26,7 @@ CarrierWave.configure do |config|
  
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
  
-  config.fog_directory    = ENV['akimono']
+  config.fog_directory    = ENV[akimono]
   # config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
   #config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 end
