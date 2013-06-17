56c784de1fb61f4c50208eb9425cd726574f9c5e
akimono <frank.cioppettini@gmail.com>
2013-06-16 22:04:26 -0400

    adslj

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,10 @@ CarrierWave.configure do |config|
 		:aws_secret_access_key => ENV[GJANA2u+uhUhOco+0X3iKobH8JSaO3yLEb12LROS],
 		:region => ENV['us-west-2']
 	}
+
 	config.cache_dir = "#{Rails.root}/tmp/uploads"
 	config.fog_directory = ENV['akimono']
 	config.s3_access_policy = :public_read
 	config.fog_host = "#{ENV['https://s3-us-west-2.amazonaws.com/akimono/']}/#{ENV['akimono']}"
+end
 end
