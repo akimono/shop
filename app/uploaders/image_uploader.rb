4e7f19811d9684896c8e0eeb300cdec9b97e0cfa
akimono <frank.cioppettini@gmail.com>
2013-06-17 15:56:26 -0400

    first try

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,12 +13,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-include CarrierWave::MiniMagick
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+
 
      # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -51,5 +49,9 @@ include CarrierWave::MiniMagick
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+def store_dir
+“akimono/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}”
+end
 
 end
