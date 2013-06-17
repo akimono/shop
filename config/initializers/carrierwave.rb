6941b55826aa91db77b9079215cf3cab87659101
akimono <frank.cioppettini@gmail.com>
2013-06-16 21:18:06 -0400

    carrierwave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+	config.fog_credentials ={
+
+		:provider => 'AWS',
+		:aws_acess_key_id => ENV[AKIAI2AVYGJ3CUGRYP6Q],
+		:aws_secret_access_key => ENV[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],
+		:region => ENV['Oregon']
+	}
+	config.cache_dir = "#{Rails.root}/tmp/uploads"
+	config.fog_directory = ENV['akimono']
+	config.s3_access_policy = :public_read
+	config.fog_host = "#{ENV['https://s3-us-west-2.amazonaws.com/akimono/'']}/#{ENV['akimono']}"
+end
