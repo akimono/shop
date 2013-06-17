d5bd4541b469cdbe1548385cdc013241404c25ef
akimono <frank.cioppettini@gmail.com>
2013-06-17 17:18:40 -0400

    seventh

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,7 +12,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   
-   storage :file
+  include CarrierWave::MiniMagick
+ 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
