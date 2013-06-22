69d4a36b501fbeb383283ba168a4e80ae3c5ec2d
akimono <frank.cioppettini@gmail.com>
2013-06-22 11:18:26 -0400

    adslfkjsflkj

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,4 +4,8 @@ def contact_email(contactor)
 	@contactor = contactor
     mail(:to => "contact@akimono.com", :subject => "Vendor Application")
 end
+def main_email(contact)
+	@contact = contact
+    mail(:to => "contact@akimono.com", :subject => "Contact")
+end
 end
